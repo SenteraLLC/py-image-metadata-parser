f615ce3fbeb063d0e21ef5705e61016633fd83ce
Elliott Imhoff <elliott.imhoff@sentera.com>
2019-08-19 14:17:27 -0500

    Functions raise ValueErrors

--- imgparse/pixel_pitches.py
+++ imgparse/pixel_pitches.py
@@ -1,6 +1,10 @@
 """
 Lookup tables for DJI and Hasselblad pixel pitches.  Neither camera types store the pixel pitch in image metadata, so
 they need to be manually coded and accessed.
+
+To add a new supported camera make, create a new dictionary of camera model/pixel pitch pairs, then add dictionary
+to PIXEL_PITCHES, indexed by camera make.
+To add a new supported camera model, simply append a new model/pixel pitch pair to the existing camera make dictionary.
 """
 
 DJI_PIXEL_PITCH = {'FC6310': 2.41e-06, 'FC6310S': 2.41e-06, 'FC220': 1.55e-06, 'FC6520': 3.4e-06, 'FC330': 1.57937e-06,
