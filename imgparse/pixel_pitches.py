fd4fafa5f93688e2028553ad99b52c4cea4d4c3b
Elliott Imhoff <elliott.imhoff@sentera.com>
2019-10-24 18:06:27 -0500

    Added more supported cameras

--- imgparse/pixel_pitches.py
+++ imgparse/pixel_pitches.py
@@ -8,7 +8,7 @@ To add a new supported camera model, simply append a new model/pixel pitch pair 
 """
 
 DJI_PIXEL_PITCH = {'FC6310': 2.41e-06, 'FC6310S': 2.41e-06, 'FC220': 1.55e-06, 'FC6520': 3.4e-06, 'FC330': 1.57937e-06,
-                   'FC300X': 1.57937e-06, 'FC300S': 1.57937e-06}
+                   'FC300X': 1.57937e-06, 'FC300S': 1.57937e-06, 'FC6510': 2.42e-06, 'FC350': 1.57937e-06}
 
 HASSELBLAD_PIXEL_PITCH = {'L1D-20c': 2.4e-06}
 
