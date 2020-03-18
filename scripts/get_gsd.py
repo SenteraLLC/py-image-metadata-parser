7f43896ce1f6de8c3efae8cfab5a8be20b048537
Elliott Imhoff <elliott.imhoff@sentera.com>
2020-03-18 11:10:18 -0500

    Added optional altitude param for get_gsd() script

--- scripts/get_gsd.py
+++ scripts/get_gsd.py
@@ -8,6 +8,11 @@ import imgparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("image_path", help="Image path to get gsd of")
+    parser.add_argument(
+        "--altitude",
+        help="(optional) Relative altitude of the image above ground",
+        type=float,
+    )
     args = parser.parse_args()
 
     # Setup logger
@@ -18,5 +23,5 @@ if __name__ == "__main__":
     root_logger.addHandler(handler)
     root_logger.setLevel(logging.INFO)
 
-    gsd = imgparse.get_gsd(args.image_path)
+    gsd = imgparse.get_gsd(args.image_path, corrected_alt=args.altitude)
     print(f"GSD: {gsd}")
