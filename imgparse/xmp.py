5c97d4bd177e6459282077b653c08ec261313044
unknown <cliff.dellamano@sentera.com>
2020-05-14 11:17:06 -0500

    More messing around

--- imgparse/xmp.py
+++ imgparse/xmp.py
@@ -1,16 +1,31 @@
+"""Extract XMP data from images."""
+
 import re
-import typing
 
 
 class CombinablePattern(re.Pattern):
+    """Not sure what this will do yet."""
 
     def __init__(self):
+        """Not sure what this will take yet."""
         pass
 
     def combined_with(self, other):
+        """
+        Not sure what this will do yet.
+
+        :param other:
+        :return:
+        """
         pass
 
 
-def compile_combinable(regex):
+def compile_combinable(regex: str) -> CombinablePattern:
+    """
+    Not sure what this will do yet.
+
+    :param regex:
+    :return:
+    """
     pattern = re.compile(regex)
     return CombinablePattern(pattern)
