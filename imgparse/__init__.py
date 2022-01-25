7366f510b99796dbecd777dd4bd09d1845aa7abf
Elliott <elliott.imhoff@sentera.com>
2022-01-24 19:55:07 -0600

    Remove deprecated comments

--- imgparse/__init__.py
+++ imgparse/__init__.py
@@ -3,20 +3,6 @@ This package contains functions to extract imagery metadata from their exif and 
 
 Supports some DJI, some Hasselblad, some Sony, and all Sentera sensors.
 
-All functions take an image path as an argument for easy use, but optionally can take the dictionary of exif and/or the
-extracted xmp string returned by ``imgparse.get_exif_data()`` and ``imgparse.get_xmp_data()``.  This allows the user to
-avoid rereading the exif/xmp data for an image when extracting multiple kinds of metadata in order to speed up
-processing.  Example code for using this functionality is shown below.
-
-.. code-block:: python
-
-    exif_data = imgparse.get_exif_data(image_path)
-    xmp_data = imgparse.get_xmp_data(image_path)
-    make, model = imgparse.get_make_and_model(exif_data=exif_data)
-    lat, lon = imgparse.get_lat_lon(exif_data=exif_data)
-    focal_length, pixel_pitch = imgparse.get_camera_params(exif_data=exif_data)
-    roll, pitch, yaw = imgparse.get_roll_pitch_yaw(exif_data=exif_data, xmp_data=xmp_data)
-
 Run ``imgparse --help`` on the command line to see all available CLI commands that are installed with the package.
 """
 
