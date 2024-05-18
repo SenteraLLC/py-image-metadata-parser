b3676ab2e4e4bf3db0bb52f52787311517db3936
Nicholas Witthoeft <49255471+njwitthoeft@users.noreply.github.com>
2024-05-17 23:47:16 -0500

    [WS] Add Serial Number Support (#74)
    * add serial no support

--- imgparse/imgparse.py
+++ imgparse/imgparse.py
@@ -70,6 +70,28 @@ def get_firmware_version(image_path, exif_data=None):
 
 
 @get_if_needed("exif_data", getter=get_exif_data, getter_args=["image_path"])
+def get_serial_number(image_path, exif_data=None):
+    """
+    Get the serial number of the sensor.
+
+    Expects serial number to be parsable as an integer.
+
+    :param image_path: the full path to the image
+    :param exif_data: used internally for memoization. Not necessary to supply.
+    :return: **serial_no** - sensor serial version
+    :raises: ParsingError
+    """
+    try:
+        serial_no = int(exif_data["Image BodySerialNumber"].values)
+    except (KeyError, ValueError):
+        raise ParsingError(
+            "Couldn't parse sensor version. Sensor might not be supported"
+        )
+
+    return serial_no
+
+
+@get_if_needed("exif_data", getter=get_exif_data, getter_args=["image_path"])
 def get_timestamp(image_path, exif_data=None, format_string="%Y:%m:%d %H:%M:%S"):
     """
     Get the time stamp of an image and parse it into a `datetime` object with the given format string.
