610f2a1ee12046e4b306c25bd22de278a4a2c2df
Joey Franck <joseph.franck@sentera.com>
2025-03-03 09:20:56 -0600

    fix type in dewarp flag, lock ^3.10

--- imgparse/parser.py
+++ imgparse/parser.py
@@ -555,7 +555,7 @@ class MetadataParser:
     def dewarp_flag(self) -> bool:
         """Get the dewarp flag of the image."""
         try:
-            return bool(self.xmp_data[self.xmp_tags.DEWARP_FLAG])
+            return bool(int(self.xmp_data[self.xmp_tags.DEWARP_FLAG]))
         except KeyError:
             raise ParsingError(
                 "Couldn't parse dewarp flag. Sensor might not be supported"
